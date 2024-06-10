698b1acd7b6919d0b71801eb3ddc3cea3fbe1a32
Chanuka999 <155728444+Chanuka999@users.noreply.github.com>
2024-06-10 12:55:14 -0700

    update

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>E commerce web site using php and mysql</title>
+    <link rel="stylesheet" href="style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
 
@@ -11,22 +12,16 @@
 
 </head>
 <body>
-    
-<div class="container-fluid">
- <nav class="navbar">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="#">Logo</a>
-        <button class="navbar-toggle">
-          <spam class="navbar-toggler-icon"></spam>
-
-        </button>
-    </div>
-
- </nav>
-
-</div>
-
-
+<nav>
+    <ul>
+      <li><a href="#" class="active">Home</a></li>
+      <li><a href="#">Product</a></li>
+      <li><a href="#">Register</a></li>
+      <li><a href="#">Contact</a></li>
+      <li id="total-price">Total Price: $0.00</li>
+    </ul>
+  </nav>
+  
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
