1f35e43ae5a40eccbbf02f35f291cddadd3c6204
Chanuka999 <155728444+Chanuka999@users.noreply.github.com>
2024-06-10 18:29:19 -0700

    updare

--- index.php
+++ index.php
@@ -12,17 +12,35 @@
 
 </head>
 <body>
+  <!--div class="container"-->
 <nav>
+
     <ul>
+   
+    <li><a href="#" class="cool">LOGO</a></li>
       <li><a href="#" class="active">Home</a></li>
       <li><a href="#">Product</a></li>
       <li><a href="#">Register</a></li>
       <li><a href="#">Contact</a></li>
-      <li id="total-price">Total Price: $0.00</li>
+      <li><a href="#"><i class="fa-solid fa-cart-shopping"></i><sup>1</sup></a></li>
+      <li><a href="#">Total Price:100/=</a></li>
+     
+    
+    <form class="kill">
+      <input class="form" type="search" placeholder="search" aria-label="search">
+      <button class="btn" type="submit">search</button>
+    </form>
     </ul>
   </nav>
   
 
+
+
+
+  <div class="footer">
+    <p>All right reserved o- designed by chanuka-2024</p>
+  </div>
+  
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
 </body>
