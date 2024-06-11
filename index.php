3ba693abb204df91b36323245be4b8f399c56a09
Chanuka999 <155728444+Chanuka999@users.noreply.github.com>
2024-06-11 14:15:20 -0700

    update

--- index.php
+++ index.php
@@ -56,7 +56,7 @@
     <div class="row">
      <div class="col-md-4 mb-2">
      <div class="card" >
-  <img src="./images/apple.webp" class="card-img-top" alt="...">
+  <img src="./images/apple.webp" class="card-img-top" alt="apple">
   <div class="card-body">
     <h5 class="card-title">Card title</h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
