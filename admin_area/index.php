720f7dd0f024a6e9ef14ea49a59c1e309113e4db
Chanuka999 <155728444+Chanuka999@users.noreply.github.com>
2024-06-12 13:46:35 -0700

    add button

--- admin_area/index.php
+++ admin_area/index.php
@@ -6,6 +6,12 @@
     <title>Admin dash board</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="../style.css">
+    <style>
+      .admin-image{
+          width: 100px;
+          object-fit: contain;
+       }
+    </style>
 </head>
 <body>
   <div class="container-fluid p-0">
@@ -28,8 +34,22 @@
     <div class="row">
         <div class="col-md-12 bg-secondary p-1">
          <div>
-            <a href="#"><img src="../images/images.jpeg" alt=""></a>
+            <a href="#"><img src="../images/images.jpeg" alt="" class="admin-image"></a>
             <p class="text-light text-center">Admin Name</p>
+         </div>
+         <!--   -->
+         <div class="button text-center">
+          <button><a href="" class="nav-link text-light bg-info my-1"></a></button>
+          <button><a href="" class="nav-link text-light bg-info my-1"></a></button>
+          <button><a href="" class="nav-link text-light bg-info my-1"></a></button>
+          <button><a href="" class="nav-link text-light bg-info my-1"></a></button>
+          <button><a href="" class="nav-link text-light bg-info my-1"></a></button>
+          <button><a href="" class="nav-link text-light bg-info my-1"></a></button>
+          <button><a href="" class="nav-link text-light bg-info my-1"></a></button>
+          <button><a href="" class="nav-link text-light bg-info my-1"></a></button>
+          <button><a href="" class="nav-link text-light bg-info my-1"></a></button>
+          <button><a href="" class="nav-link text-light bg-info my-1"></a></button>
+
          </div>
         </div>
     </div>
