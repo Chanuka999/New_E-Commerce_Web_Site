e21d81b247c631d02b5c8e75f26e96c060013435
Chanuka999 <155728444+Chanuka999@users.noreply.github.com>
2024-06-13 08:18:29 -0700

    update

--- admin_area/index.php
+++ admin_area/index.php
@@ -6,6 +6,8 @@
     <title>Admin dash board</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="../style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
     <style>
       .admin-image{
           width: 100px;
@@ -46,7 +48,7 @@
          <div class="button text-center">
           <button class="my-3"><a href="" class="nav-link text-light bg-info my-1 ">Insert Products</a></button>
           <button><a href="" class="nav-link text-light bg-info my-1">View Products</a></button>
-          <button><a href="insert_categories.php" class="nav-link text-light bg-info my-1">Insert Categories</a></button>
+          <button><a href="index.php?insert_category" class="nav-link text-light bg-info my-1">Insert Categories</a></button>
           <button><a href="" class="nav-link text-light bg-info my-1">View Categories</a></button>
           <button><a href="" class="nav-link text-light bg-info my-1">Insert Brands</a></button>
           <button><a href="" class="nav-link text-light bg-info my-1">View Brands</a></button>
