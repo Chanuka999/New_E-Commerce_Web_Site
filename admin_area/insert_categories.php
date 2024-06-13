c5e530427d49e09a6c7fdca3e150b6e1163b6f6e
Chanuka999 <155728444+Chanuka999@users.noreply.github.com>
2024-06-13 08:41:42 -0700

    update

--- admin_area/insert_categories.php
+++ admin_area/insert_categories.php
@@ -4,10 +4,10 @@
         <input type="text" class="form-contrall" name="cat_title" placeholder="insert categories" aria-label="username" aria-describedby="basic-addon1">
 
     </div> 
-    <div class="input-group w-10 mb-2">
+    <div class="input-group w-10 mb-2 m-auto">
        
-        <input type="submit" class="form-contrall" name="insert_cat" value="insert categories" aria-label="username" aria-describedby="basic-addon1" class="bg-info">
-        
+        <!-- <input type="submit" class="form-contrall" name="insert_cat" value="insert categories"> -->
+      <button class="bg-info p-2 my-3 border-0">Insert Categories</button>
 
     </div> 
 </form>
