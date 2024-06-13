1a03f6b77acfb8a9b9e752efa99d49c25bd2df5c
Chanuka999 <155728444+Chanuka999@users.noreply.github.com>
2024-06-13 08:19:04 -0700

    apply button

---
+++ admin_area/insert_categories.php
@@ -0,0 +1,13 @@
+<form action="" method="post" class="mb-2">
+    <div class="input-group w-90 mb-2">
+        <span class="input-group-text bg-info" id="basic-addon1"><i class="fa-solid fa-receipt"></i></span>
+        <input type="text" class="form-contrall" name="cat_title" placeholder="insert categories" aria-label="username" aria-describedby="basic-addon1">
+
+    </div> 
+    <div class="input-group w-10 mb-2">
+       
+        <input type="submit" class="form-contrall" name="insert_cat" value="insert categories" aria-label="username" aria-describedby="basic-addon1" class="bg-info">
+        
+
+    </div> 
+</form>
