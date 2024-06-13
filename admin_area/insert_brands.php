b37eb245bdb9966adc6182c706dbbe0e0caec42c
Chanuka999 <155728444+Chanuka999@users.noreply.github.com>
2024-06-13 08:51:49 -0700

    update

---
+++ admin_area/insert_brands.php
@@ -0,0 +1,13 @@
+<form action="" method="post" class="mb-2">
+    <div class="input-group w-90 mb-2">
+        <span class="input-group-text bg-info" id="basic-addon1"><i class="fa-solid fa-receipt"></i></span>
+        <input type="text" class="form-contrall" name="cat_title" placeholder="insert Brands" aria-label="brands" aria-describedby="basic-addon1">
+
+    </div> 
+    <div class="input-group w-10 mb-2 m-auto">
+       
+        <!-- <input type="submit" class="form-contrall" name="insert_cat" value="insert categories"> -->
+      <button class="bg-info p-2 my-3 border-0">Insert Brands</button>
+
+    </div> 
+</form>
