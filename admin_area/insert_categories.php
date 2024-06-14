9df7f3629f7ce56bfe1efa854ce6a7c462e2335b
Chanuka999 <155728444+Chanuka999@users.noreply.github.com>
2024-06-14 12:57:02 -0700

    update

--- admin_area/insert_categories.php
+++ admin_area/insert_categories.php
@@ -2,6 +2,13 @@
 include('../includes/connect.php');
 if(isset($_POST['insert_cat'])){
    $category_title=$_POST['cat_title'];
+
+   $select_query="select * from `categories` where category_title='$category_title'";
+   $result_select=mysqli_query($con,$select_query);
+   $number=mysqli_num_rows($result_select);
+   if($number>0){
+    echo "<script>alert('this category is present inside the database')</script>";
+   }
    $insert_query="insert into `categories` (category_title) values ('$category_title')";
    $result=mysqli_query($con,$insert_query);
    if($result){
