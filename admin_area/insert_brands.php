cd638ca871c2872e457fb80f32c3922e90bab365
Chanuka999 <155728444+Chanuka999@users.noreply.github.com>
2024-06-14 18:54:08 -0700

    update php code

--- admin_area/insert_brands.php
+++ admin_area/insert_brands.php
@@ -1,18 +1,18 @@
 <?php  
 include('../includes/connect.php');
-if(isset($_POST['insert_cat'])){
-   $category_title=$_POST['cat_title'];
+if(isset($_POST['insert_brand'])){
+   $brand_title=$_POST['brand_title'];
 
-   $select_query="select * from `categories` where category_title='$category_title'";
+   $select_query="select * from `brands` where brand_title='$brand_title'";
    $result_select=mysqli_query($con,$select_query);
    $number=mysqli_num_rows($result_select);
    if($number>0){
-    echo "<script>alert('this category is present inside the database')</script>";
+    echo "<script>alert('this brand is present inside the database')</script>";
    }else{
-   $insert_query="insert into `categories` (category_title) values ('$category_title')";
+   $insert_query="insert into `brands` (brand_title) values ('$brand_title')";
    $result=mysqli_query($con,$insert_query);
    if($result){
-    echo "<script>alert('category has been inserted successfully')</script>";
+    echo "<script>alert('brand has been inserted successfully')</script>";
    }
 }
 }
@@ -23,7 +23,7 @@ if(isset($_POST['insert_cat'])){
 <form action="" method="post" class="mb-2">
     <div class="input-group w-90 mb-2">
         <span class="input-group-text bg-info" id="basic-addon1"><i class="fa-solid fa-receipt"></i></span>
-        <input type="text" class="form-contrall" name="cat_title" placeholder="insert Brands" aria-label="brands" aria-describedby="basic-addon1">
+        <input type="text" class="form-contrall" name="brand_title" placeholder="insert Brands" aria-label="brands" aria-describedby="basic-addon1">
 
     </div> 
     <div class="input-group w-10 mb-2 m-auto">
