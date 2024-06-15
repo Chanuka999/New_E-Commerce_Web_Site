dfbb4f510904a1b5045a7f6842215bfa648ffcec
Chanuka999 <155728444+Chanuka999@users.noreply.github.com>
2024-06-14 18:42:18 -0700

    apply php code

--- admin_area/insert_brands.php
+++ admin_area/insert_brands.php
@@ -1,3 +1,25 @@
+<?php  
+include('../includes/connect.php');
+if(isset($_POST['insert_cat'])){
+   $category_title=$_POST['cat_title'];
+
+   $select_query="select * from `categories` where category_title='$category_title'";
+   $result_select=mysqli_query($con,$select_query);
+   $number=mysqli_num_rows($result_select);
+   if($number>0){
+    echo "<script>alert('this category is present inside the database')</script>";
+   }else{
+   $insert_query="insert into `categories` (category_title) values ('$category_title')";
+   $result=mysqli_query($con,$insert_query);
+   if($result){
+    echo "<script>alert('category has been inserted successfully')</script>";
+   }
+}
+}
+
+
+?>
+
 <form action="" method="post" class="mb-2">
     <div class="input-group w-90 mb-2">
         <span class="input-group-text bg-info" id="basic-addon1"><i class="fa-solid fa-receipt"></i></span>
