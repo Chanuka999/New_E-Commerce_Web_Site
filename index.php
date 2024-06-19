bd53c786259a123ca43c56e912d51c2e38245542
Chanuka999 <155728444+Chanuka999@users.noreply.github.com>
2024-06-19 00:38:29 -0700

    apply code

--- index.php
+++ index.php
@@ -101,20 +101,20 @@ include('functions/common_function.php');
         <a href="#" class="nav-link text-light"><h4>Delivery Brands</h4></a>
       </li>
       <?php   
-      
-      $select_brands="Select * from `brands`";
-      $result_brands=mysqli_query($con,$select_brands);
-     // $row_data=mysqli_fetch_assoc($result_brands);
-     // echo $row_data['brand_title'];
-     // echo $row_data['brand_title'];
-    while($row_data=mysqli_fetch_assoc($result_brands)){
-      $brand_title=$row_data['brand_title'];
-      $brand_id=$row_data['brand_id'];
-       echo "<li class='nav-item'>
-       <a href='index.php?brand=$brand_id' class='nav-light text-light'>$brand_title</a>
-        </li>";
+      getbrands();
+    //   $select_brands="Select * from `brands`";
+    //   $result_brands=mysqli_query($con,$select_brands);
+    //  // $row_data=mysqli_fetch_assoc($result_brands);
+    //  // echo $row_data['brand_title'];
+    //  // echo $row_data['brand_title'];
+    // while($row_data=mysqli_fetch_assoc($result_brands)){
+    //   $brand_title=$row_data['brand_title'];
+    //   $brand_id=$row_data['brand_id'];
+    //    echo "<li class='nav-item'>
+    //    <a href='index.php?brand=$brand_id' class='nav-light text-light'>$brand_title</a>
+    //     </li>";
      
-     }
+    //  }
       
       ?>
     
@@ -123,20 +123,7 @@ include('functions/common_function.php');
         <a href="#" class="nav-link text-light "><h4>categories</h4></a>
       </li>
       <?php   
-      
-      $select_categories="Select * from `categories`";
-      $result_categories=mysqli_query($con,$select_categories);
-     // $row_data=mysqli_fetch_assoc($result_brands);
-     // echo $row_data['brand_title'];
-     // echo $row_data['brand_title'];
-    while($row_data=mysqli_fetch_assoc($result_categories)){
-      $category_title=$row_data['category_title'];
-      $category_id=$row_data['category_id'];
-       echo "<li class='nav-item'>
-       <a href='index.php?category=$category_id' class='nav-light text-light'>$category_title</a>
-        </li>";
-     
-     }
+        getcategories()
       
       ?>
 
