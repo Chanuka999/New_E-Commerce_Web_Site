06b389a5b2595f4b7f82c0b8717b411a7de3bf06
Chanuka999 <155728444+Chanuka999@users.noreply.github.com>
2024-06-20 00:52:04 -0700

    getting unique category

--- functions/common_function.php
+++ functions/common_function.php
@@ -6,7 +6,7 @@ function getproducts(){
     global $con;
 
     if(!isset($_GET['category'])){
-    if(!isset($_GET['brands'])){
+    if(!isset($_GET['brand'])){
     $select_query="Select  * from `products` order by rand() limit 0,9";
 $result_query=mysqli_query($con,$select_query);
 //$row=mysqli_fetch_assoc($result_query);
@@ -36,6 +36,44 @@ while($row=mysqli_fetch_assoc($result_query)){
 }
 }
 }
+
+
+
+
+function get_unique_categories(){
+  global $con;
+
+  if(isset($_GET['category'])){
+   $category_id=$_GET['category'];
+  $select_query="Select  * from `products` where category_id=$category_id";
+$result_query=mysqli_query($con,$select_query);
+//$row=mysqli_fetch_assoc($result_query);
+//echo $row['product_title'];
+while($row=mysqli_fetch_assoc($result_query)){
+$product_id=$row['product_id'];
+$product_title=$row['product_title'];
+$product_disription=$row['product_disription'];
+$product_image1=$row['product_image1'];
+$product_price=$row['product_price'];
+$category_id=$row['category_id'];
+$brand_id=$row['brand_id'];
+echo " <div class='col-md-4 mb-2'>
+   <div class='card' >
+    <img src='./admin_area/product_images/$product_image1' height:200px; class='card-img-top' alt='$product_title'>
+<div class='card-body'>
+  <h5 class='card-title'>$product_title</h5>
+  <p class='card-text'>$product_disription</p>
+ <a href='#' class='btn btn-info'>Add to cart</a>
+  <a href='#' class='btn btn-secondary'>view more</a>
+</div>
+</div>
+</div>
+";
+
+}
+}
+}
+
 
 function getbrands(){
     global $con;
