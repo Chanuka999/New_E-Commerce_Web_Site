2175d0f30d8168a3aa400a460c1c0451101201af
Chanuka999 <155728444+Chanuka999@users.noreply.github.com>
2024-06-20 18:37:59 -0700

    add to grt_unique_brands

--- functions/common_function.php
+++ functions/common_function.php
@@ -48,7 +48,7 @@ function get_unique_categories(){
   $select_query="Select  * from `products` where category_id=$category_id";
 $result_query=mysqli_query($con,$select_query);
 $num_of_rows=mysqli_num_rows($result_query);
-if($num_of_rows){
+if($num_of_rows==0){
   echo "<h2 class='text-center  text-danger' >No stack for the category</h2>";
 }
 
@@ -80,6 +80,47 @@ echo " <div class='col-md-4 mb-2'>
 }
 }
 
+
+
+function get_unique_brands(){
+  global $con;
+
+  if(isset($_GET['brand'])){
+   $brand_id=$_GET['brand'];
+  $select_query="Select  * from `products` where category_id=$brand_id";
+$result_query=mysqli_query($con,$select_query);
+$num_of_rows=mysqli_num_rows($result_query);
+if($num_of_rows==0){
+  echo "<h2 class='text-center  text-danger' >This  brand is not available for service</h2>";
+}
+
+
+//$row=mysqli_fetch_assoc($result_query);
+//echo $row['product_title'];
+while($row=mysqli_fetch_assoc($result_query)){
+$product_id=$row['product_id'];
+$product_title=$row['product_title'];
+$product_disription=$row['product_disription'];
+$product_image1=$row['product_image1'];
+$product_price=$row['product_price'];
+$category_id=$row['category_id'];
+$brand_id=$row['brand_id'];
+echo " <div class='col-md-4 mb-2'>
+   <div class='card' >
+    <img src='./admin_area/product_images/$product_image1' height:200px; class='card-img-top' alt='$product_title'>
+<div class='card-body'>
+  <h5 class='card-title'>$product_title</h5>
+  <p class='card-text'>$product_disription</p>
+ <a href='#' class='btn btn-info'>Add to cart</a>
+  <a href='#' class='btn btn-secondary'>view more</a>
+</div>
+</div>
+</div>
+";
+
+}
+}
+}
 
 function getbrands(){
     global $con;
