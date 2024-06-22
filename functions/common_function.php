6cd8b445582d996390ccd558aaa416d8e67d2c9f
Chanuka999 <155728444+Chanuka999@users.noreply.github.com>
2024-06-22 13:42:27 -0700

    update

--- functions/common_function.php
+++ functions/common_function.php
@@ -229,4 +229,60 @@ function getcategories(){
    
    }
 }
+
+function view_details(){
+  global $con;
+
+  if(isset($_GET['$product_id'])){
+  if(!isset($_GET['category'])){
+  if(!isset($_GET['brand'])){
+    $product_id=$_GET['$product_id'];
+  $select_query="Select  * from `products` where product_id=$product_id";
+$result_query=mysqli_query($con,$select_query);
+//$row=mysqli_fetch_assoc($result_query);
+//echo $row['product_title'];
+while($row=mysqli_fetch_assoc($result_query)){
+$product_id=$row['product_id'];
+$product_title=$row['product_title'];
+$product_disription=$row['product_disription'];
+$product_image1=$row['product_image1'];
+$product_image2=$row['product_image2'];
+$product_image3=$row['product_image3'];
+$product_price=$row['product_price'];
+$category_id=$row['category_id'];
+$brand_id=$row['brand_id'];
+echo " <div class='col-md-4 mb-2'>
+   <div class='card' >
+    <img src='./admin_area/product_images/$product_image1' height:200px; class='card-img-top' alt='$product_title'>
+<div class='card-body'>
+  <h5 class='card-title'>$product_title</h5>
+  <p class='card-text'>$product_disription</p>
+ <a href='#' class='btn btn-info'>Add to cart</a>
+  <a href='product_details.php?product_id=$product_id' class='btn btn-secondary'>view more</a>
+</div>
+</div>
+</div>
+
+ <div class='col-md-8'>
+             <div class='row'>
+                <div class='col-md-12'>
+                   <h4 class='text-center text-info mb-5'>Related products</h4> 
+                </div>
+                <div class='col-md-6'>
+                <img src='./admin_area/product_images/$product_image2' class='card-img-top' alt='$product_title'>
+
+                </div>
+                <div class='col-md-6'>
+                <img src='./admin_area/product_images/$product_image3' class='card-img-top' alt='$product_title'>
+                </div>
+             </div>
+        </div>";
+
+}
+}
+}
+}
+}
+
+
 ?>
