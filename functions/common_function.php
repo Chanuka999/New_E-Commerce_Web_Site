de61c81f7661d0f8d83d67e5d117d3e4649d8771
Chanuka999 <155728444+Chanuka999@users.noreply.github.com>
2024-06-21 18:31:06 -0700

    apply function

--- functions/common_function.php
+++ functions/common_function.php
@@ -157,6 +157,45 @@ echo " <div class='col-md-4 mb-2'>
 }
 }
 
+function search_product(){
+  global $con;
+
+  if(isset($_GET['searc_data_product'])){
+   $search_data_value=$_GET['search_data'];
+  $select_query="Select  * from `products` where product_keywords like '%value%'";
+$result_query=mysqli_query($con,$select_query);
+$num_of_rows=mysqli_num_rows($result_query);
+if($num_of_rows==0){
+  echo "<h2 class='text-center  text-danger' >No stack for the category</h2>";
+}
+
+
+//$row=mysqli_fetch_assoc($result_query);
+//echo $row['product_title'];
+while($row=mysqli_fetch_assoc($result_query)){
+$product_id=$row['product_id'];
+$product_title=$row['product_title'];
+$product_disription=$row['product_disription'];
+$product_image1=$row['product_image1'];
+$product_price=$row['product_price'];
+$category_id=$row['category_id'];
+$brand_id=$row['brand_id'];
+echo " <div class='col-md-4 mb-2'>
+   <div class='card' >
+    <img src='./admin_area/product_images/$product_image1' height:200px; class='card-img-top' alt='$product_title'>
+<div class='card-body'>
+  <h5 class='card-title'>$product_title</h5>
+  <p class='card-text'>$product_disription</p>
+ <a href='#' class='btn btn-info'>Add to cart</a>
+  <a href='#' class='btn btn-secondary'>view more</a>
+</div>
+</div>
+</div>
+";
+
+}
+}
+}
 function getbrands(){
     global $con;
     $select_brands="Select * from `brands`";
