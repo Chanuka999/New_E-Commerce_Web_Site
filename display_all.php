4f3bcd3379e20a2fed490a26a7a0f4f12f8cea66
Chanuka999 <155728444+Chanuka999@users.noreply.github.com>
2024-06-23 16:13:18 -0700

    add cart price

--- display_all.php
+++ display_all.php
@@ -29,7 +29,7 @@ include('functions/common_function.php');
       <li><a href="#">Register</a></li>
       <li><a href="#">Contact</a></li>
       <li><a href="#"><i class="fa-solid fa-cart-shopping"></i><sup>1</sup></a></li>
-      <li><a href="#">Total Price:100/=</a></li>
+      <li><a href="#">Total Price:<?php total_cart_price(); ?></a></li>
      
     
     <form class="kill">
