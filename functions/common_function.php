79a0a2dcf31b73085ff2185dd3c0d650040607a0
Chanuka999 <155728444+Chanuka999@users.noreply.github.com>
2024-06-23 11:16:55 -0700

    update

--- functions/common_function.php
+++ functions/common_function.php
@@ -306,11 +306,22 @@ function getIPAddress() {
 function cart(){
   if(isset($_GET['add_to_cart'])){
     global $con;
-    $ip = getIPAddress(); 
+    $get_ip_add = getIPAddress(); 
     $get_product_id=$_GET['add_to_cart'];
-    $select_query="Select * from `cart_details` where ip_address=$ip and
-    product_id=$get_product_id";
+    $select_query="Select * from `cart_details` where ip_address='$get_ip_add' and product_id=$get_product_id";
     $result_query=mysqli_query($con,$select_query);
+    $num_of_rows=mysqli_num_rows($result_query);
+if($num_of_rows>0){
+  echo "<script>alert('This item is already present inside card')</script>";
+  echo "<script>window.open('index.php','_self')</script>";
+}else{
+  $insert_query="insert into `cart_details` (product_id,ip_address,quantity) values($get_product_id,'$get_ip_add',0)";
+  $result_query=mysqli_query($con,$insert_query);
+  echo "<script>alert('Item is added to card')</script>";
+  echo "<script>window.open('index.php','_self')</script>";
+
+
+}
   }
 }
 ?>
