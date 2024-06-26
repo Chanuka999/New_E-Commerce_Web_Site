1f28c4e5665cdceacf733dccf7da266618569b35
Chanuka999 <155728444+Chanuka999@users.noreply.github.com>
2024-06-26 07:57:34 -0700

    user login form create

--- users_area/user_login.php
+++ users_area/user_login.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>addition</title>
+    <title>User - registration</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
 
@@ -11,7 +11,33 @@
 
 </head>
 <body>
-    <h1>User Login</h1>
-    
+    <div class="container-fluid my-3">
+        <h2 class="text-center">User Logn</h2>
+        <div class="row d-flex align-item-center justify-content-center mt-5">
+            <div class="lg-12 col-xl-6">
+                <form cavtion="" method="post" enctype="multipart/form_data">
+                    <div class="form-outline mb-4">
+                     <label for="user_username" class="form_label">Username</label>  
+                     <input type="text" id="user_username" class="form-control" placeholder="Enter your username" autocomplete="off" required="required" name="user_username">
+                    </div>
+
+                    
+                    <div class="form-outline mb-4">
+                    <label for="user_password" class="form_label">Password</label>  
+                     <input type="password" id="user_password" class="form-control"  required="required" name="user_paasword">
+                    </div>
+
+
+                    <div class="mt-4 pt-2">
+                        <input type="submit" value="Login" class="bg-info py-2 px-3 border-0" name="user_login">
+                        <p class="small fw-bold mt-2 pt-1 mb-0">Dont't have an account ?<a href="user_registration.php" class="text-danger"> Login</a></p>
+                    </div>
+                </form>
+
+            
+
+            </div>
+        </div>
+    </div>
 </body>
 </html>
