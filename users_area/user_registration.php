232b9a5c98f33459789d46d41054e7147162b2c5
Chanuka999 <155728444+Chanuka999@users.noreply.github.com>
2024-06-26 00:37:27 -0700

    update

--- users_area/user_registration.php
+++ users_area/user_registration.php
@@ -4,8 +4,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>User - registration</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
 </head>
 <body>
-    
+    <div class="container-fluid my-3">
+        <h2 class="text-center">New User Registation</h2>
+        <div class="row">
+            <div class="lg-12 col-xl-6">
+                <form cavtion="" method="post" anctype="multipart/form_data">
+                    <div class="form-outline"></div>
+                </form>
+
+            
+
+            </div>
+        </div>
+    </div>
 </body>
 </html>
