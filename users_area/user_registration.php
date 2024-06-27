0435929c74da177240e50f025e7b1776ae45899e
Chanuka999 <155728444+Chanuka999@users.noreply.github.com>
2024-06-27 00:35:03 -0700

    update

--- users_area/user_registration.php
+++ users_area/user_registration.php
@@ -1,3 +1,7 @@
+<?php include('../includes/connect.php');
+ include('../functions/common_function.php');
+   ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -66,3 +70,21 @@
     </div>
 </body>
 </html>
+
+<?php  
+if(isset($_POST['user_register'])){
+    $user_username=$_POST['user_username'];
+    $user_email=$_POST['user_email'];
+    $user_password=$_POST['user_password'];
+    $conf_user_username=$_POST['conf_user_username'];
+    $user_address=$_POST['user_address'];
+    $user_email=$_POST['user_contact'];
+    $user_image=$_FILES['user_image']['name'];
+    $user_image_tmo=$_FILES['user_image']['tmp_name'];
+    $user_id=getIPAddress();
+    
+}
+
+
+
+?>
