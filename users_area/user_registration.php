27ebed5a88af80e93ee7d3a3bdc483626656a371
Chanuka999 <155728444+Chanuka999@users.noreply.github.com>
2024-06-27 07:56:21 -0700

    connect to php

--- users_area/user_registration.php
+++ users_area/user_registration.php
@@ -78,11 +78,21 @@ if(isset($_POST['user_register'])){
     $user_password=$_POST['user_password'];
     $conf_user_username=$_POST['conf_user_username'];
     $user_address=$_POST['user_address'];
-    $user_email=$_POST['user_contact'];
+    $user_contact=$_POST['user_contact'];
     $user_image=$_FILES['user_image']['name'];
-    $user_image_tmo=$_FILES['user_image']['tmp_name'];
+    $user_image_tmp=$_FILES['user_image']['tmp_name'];
     $user_id=getIPAddress();
     
+    move_uploaded_file($user_image_tmp,"./user_images/$user_image");
+    $insert_query="insert into `user_table` (username,user_email,user_password,user_image,user_ip,user_address_user_mobile) values('$user_username','$user_email','$user_password','$user_image','$user_ip','$user_address','$user_contact')";
+    $sql_execute=mysqli_query($con,$insert_query);
+    if($sql_execute){
+        echo "<script>alert('Data inserted successfully')</script>";
+    }else{
+        die(mysqli_error($con));
+    }
+
+
 }
 
 
