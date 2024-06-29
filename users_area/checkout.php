b1450d2e035f82dd0f00ef58017910fad2044d7d
Chanuka999 <155728444+Chanuka999@users.noreply.github.com>
2024-06-29 10:38:40 -0700

    add to link for registration button

--- users_area/checkout.php
+++ users_area/checkout.php
@@ -68,7 +68,7 @@ include('../includes/connect.php');
    if(!isset($_SESSION['username'])){
     include('user_login.php');
    }else{
-    include('../payment.php');
+    include('payment.php');
    }
    
    ?>
