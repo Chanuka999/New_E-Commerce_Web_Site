73afadd19f7fc926eaca394d8e0b990a49385581
chanuka999 <chanukaranditha99/2gmail.com>
2024-07-01 07:49:37 -0700

    new changes

---
+++ users_area/order.php
@@ -0,0 +1,19 @@
+<?php 
+
+include('../includes/connect.php');
+include('../functions/common_function.php');
+
+if(isset($_GET['user_id'])){
+    $user_id=$_GET['user_id'];
+}
+
+$get_ip_address=getIPAddress();
+$total_price=0;
+$cart_query_price="Select * from `cart_details` where ip_address=''";
+$result_cart_price=mysqli_query($con,$cart_query_price);
+$count_product=mysqli_num_rows($result_cart_price);
+while($row_price=mysqli_fetch_array($result_cart_price)){
+    $product_id=$row_price['product_id'];
+}
+
+?>
