8cca05df122a86d8eaeea2b2beebf9104c9aaf10
chanuka999 <chanukaranditha99@gmail.com>
2024-07-05 19:33:17 -0700

    update code

--- users_area/user_registration.php
+++ users_area/user_registration.php
@@ -15,46 +15,81 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
 </head>
+<style>
+body{
+        overflow-x: hidden;
+        background-image:url('../images/logopic4.webp');
+        background-repeat: no-repeat;
+        background-size:cover;
+       
+        
+    }
+    .container{
+        box-shadow: 
+    0px 2px 10px rgba(0,0,0,0.2), 
+    0px 10px 20px rgba(0,0,0,0.3), 
+    0px 30px 60px 1px rgba(0,0,0,0.5);
+  border-radius: 8px;
+  padding: 50px;
+  /*-fluid my-3*/
+  margin-top: 3%;
+  position:relative;
+  width:800px;
+  height: 840px;
+  background: transparent;
+  backdrop-filter: blur(10px);
+  
+
+  
+  
+    }
+    .form{
+        text-decoration: double;
+        color: black;
+        text-size-adjust:initial;
+        
+    }
+    </style>
 <body>
-    <div class="container-fluid my-3">
+    <div class="container">
         <h2 class="text-center">New User Registation</h2>
         <div class="row d-flex align-item-center justify-content-center">
             <div class="lg-12 col-xl-6">
                 <form cavtion="" method="post" enctype="multipart/form_data">
                     <div class="form-outline mb-4">
-                     <label for="user_username" class="form_label">Username</label>  
+                     <label for="user_username" class="form">Username</label>  
                      <input type="text" id="user_username" class="form-control" placeholder="Enter your username" autocomplete="off" required="required" name="user_username">
                     </div>
 
                     <div class="form-outline mb-4">
-                    <label for="user_email" class="form_label">Email</label>  
+                    <label for="user_email" class="form">Email</label>  
                      <input type="email" id="user_email" class="form-control" placeholder="Enter your email" autocomplete="off" required="required" name="user_email">
                     </div>
 
                     <div class="form-outline mb-4">
-                        <label for="user_image" class="form-label">User Image</label>  
+                        <label for="user_image" class="form">User Image</label>  
                         <input type="file" id="user_image" class="form-control" required="required" name="user_image">
                     </div>
 
                     <div class="form-outline mb-4">
-                    <label for="user_password" class="form_label">Password</label>  
+                    <label for="user_password" class="form">Password</label>  
                      <input type="password" id="user_password" class="form-control"  required="required" name="user_password">
                     </div>
 
                     <div class="form-outline mb-4">
-                    <label for="conf_user_password" class="form_label">Confirm Password</label>  
+                    <label for="conf_user_password" class="form">Confirm Password</label>  
                      <input type="password" id="conf_user_password" class="form-control" placeholder="confirm password"  required="required" name="conf_user_password">
                     </div>
 
                    
                     <div class="form-outline mb-4">
-                     <label for="user_address" class="form_label">Address</label>  
+                     <label for="user_address" class="form">Address</label>  
                      <input type="text" id="user_address" class="form-control" placeholder="Enter your username" autocomplete="off" required="required" name="user_address">
                     </div>
 
                     
                     <div class="form-outline mb-4">
-                     <label for="user_contact" class="form_label">Contact</label>  
+                     <label for="user_contact" class="form">Contact</label>  
                      <input type="text" id="user_contact" class="form-control" placeholder="Enter your mobile number" autocomplete="off" required="required" name="user_contact">
                     </div>
 
@@ -72,33 +107,6 @@
 </body>
 </html>
 
-<!--?php  
-if(isset($_POST['user_register'])){
-    $user_username=$_POST['user_username'];
-    $user_email=$_POST['user_email'];
-    $user_password=$_POST['user_password'];
-    $conf_user_password=$_POST['conf_user_password'];
-    $user_address=$_POST['user_address'];
-    $user_contact=$_POST['user_contact'];
-    $user_image=$_FILES['user_image']['name'];
-    $user_image_tmp=$_FILES['user_image']['tmp_name'];
-    $user_ip=getIPAddress();
-    
-    move_uploaded_file($user_image_tmp,"../user_images/$user_image");
-    $insert_query="insert into `user_table` (username,user_email,user_password,user_image,user_ip,user_address_user_mobile) values ('$user_username','$user_email','$user_password','$user_image','$user_ip','$user_address','$user_contact')";
-    $sql_execute=mysqli_query($con,$insert_query);
-    if($sql_execute){
-        echo "<script>alert('Data inserted successfully')</script>";
-    }else{
-        die(mysqli_error($con));
-    }
-
-
-}
-
-
-
-?--> 
 <?php
 
 if(isset($_POST['user_register'])){
